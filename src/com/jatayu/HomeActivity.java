02e90a78308ff085cb310361016e24e8aa7c3ca8
Neeraj Sharma <sharman@digitalcyclone.com>
2011-08-16 16:25:57 -0500

    In MinneknowTActivity set layout to home rather than splash. The idea is to avaoid the concept of splash screen

---
+++ src/com/jatayu/HomeActivity.java
@@ -0,0 +1,7 @@
+package com.jatayu;
+
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+}
--- src/com/jatayu/MinneknowTActivity.java
+++ src/com/jatayu/MinneknowTActivity.java
@@ -9,7 +9,8 @@ public class MinneknowTActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.splashlayout);
+
+		setContentView(R.layout.home);
 	}
 
 }
