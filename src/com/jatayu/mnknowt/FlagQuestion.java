04ecbd7d1b9a59c5ac0b7ef62f751f08f7d25b09
Neeraj Sharma <sharman@digitalcyclone.com>
2012-01-09 13:37:31 -0600

    added images for flash card flag and modified flash cards layout

---
+++ src/com/jatayu/mnknowt/FlagQuestion.java
@@ -0,0 +1,12 @@
+package com.jatayu.mnknowt;
+
+import java.util.HashMap;
+
+public class FlagQuestion {
+
+	private HashMap<String, Boolean>	flagQs_map;
+
+	public FlagQuestion() {
+		flagQs_map = new HashMap<String, Boolean>();
+	}
+}
